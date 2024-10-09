cd08043215d4d633c8c948201cc07a5eff287f63
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2024-10-09 12:04:02 +0530

    Updated CORS

--- server/index.js
+++ server/index.js
@@ -24,7 +24,13 @@ const httpServer = createServer(app);
 initSocket(httpServer);
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST","PUT","DELETE"],
+    credentials: true,
+  }
+));
 app.use(morgan("dev"));
 app.use(express.json());
 
